dabe519e8b7d66ba1daebb94af10c0992d4ad9f8
Thai Hoang Minh Tam <tam.thaihoangminh@vn.bosch.com>
2023-07-26 23:16:18 +0700

    Can load the camera

--- yolov8-tfjs.js
+++ yolov8-tfjs.js
@@ -41,7 +41,6 @@ const YOLOv8_TFJS = async (box) => {
     main_container.className = "flex w-full h-full overflow-hidden";
 
     // Create elements as in your React code.
-    // You would need to implement listeners, add appropriate styles, and adjust to suit your needs.
     main_container.innerHTML = `
         <div>
             <h1>📷 YOLOv8 Live Detection App</h1>
@@ -51,13 +50,59 @@ const YOLOv8_TFJS = async (box) => {
         <div>
             <img id="image" src="#" />
             <video id="camera" autoplay muted></video>
-            <video id="video" autoplay muted></video>
+            <canvas id="canvas"></canvas>
         </div>
         <button id="button-handler">Handle</button>
     `;
 
     box.injectNode(main_container);
-}
+
+    // Access the video and canvas elements
+    const video = main_container.querySelector('video');
+    const canvas = main_container.querySelector("canvas");
+    const ctx = canvas.getContext("2d");
+
+    // Function to run detection on video frames
+    const detectWebcam = async () => {
+        if (video.readyState === 4) {
+            // Set canvas dimensions
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+
+            // Run detection and draw results on the canvas
+            const results = await detect(video, yolov8, ctx);
+            // Assuming "detect" function will return bounding boxes and labels, etc.
+            results.forEach(result => {
+                // Assuming each result has a bounding box and label
+                ctx.beginPath();
+                ctx.rect(result.bbox[0], result.bbox[1], result.bbox[2], result.bbox[3]);
+                ctx.lineWidth = 2;
+                ctx.strokeStyle = "red";
+                ctx.fillStyle = "red";
+                ctx.stroke();
+                ctx.fillText(
+                    result.label,
+                    result.bbox[0],
+                    result.bbox[1] > 10 ? result.bbox[1] - 5 : 10
+                );
+            });
+
+            // Call this function again to detect the next frame
+            window.requestAnimationFrame(detectWebcam);
+        }
+    };
+
+    // Get video from webcam
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+            video.srcObject = stream;
+            video.onloadedmetadata = () => {
+                video.play();
+                // Start detection once video is loaded
+                window.requestAnimationFrame(detectWebcam);
+            };
+        });
+};
 
 const plugin = async ({ widgets, simulator, vehicle }) => {
     widgets.register('YOLOv8_TFJS_Widget', (box) => { YOLOv8_TFJS(box) })
