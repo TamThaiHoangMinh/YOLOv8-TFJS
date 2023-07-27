425cc5e9de12ea964e06d86400e2acbda315aabc
Tam Thai Hoang Minh <115630638+TamThaiHoangMinh@users.noreply.github.com>
2023-07-27 12:29:02 +0700

    No error but can't draw boundingbox

--- yolov8-tfjs.js
+++ yolov8-tfjs.js
@@ -87,6 +87,106 @@ const YOLOv8_TFJS = async (box) => {
 
     await getLabels();
 
+    const renderBoxes = (canvasRef, boxes_data, scores_data, classes_data, ratios) => {
+        const ctx = canvasRef.getContext("2d");
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // clean canvas
+      
+        const colors = new Colors();
+      
+        // font configs
+        const font = `${Math.max(
+          Math.round(Math.max(ctx.canvas.width, ctx.canvas.height) / 40),
+          14
+        )}px Arial`;
+        ctx.font = font;
+        ctx.textBaseline = "top";
+
+        for (let i = 0; i < scores_data.length; ++i) {
+          // filter based on class threshold
+          const klass = labels[classes_data[i]];
+          const color = colors.get(classes_data[i]);
+          const score = (scores_data[i] * 100).toFixed(1);
+
+          console.log("score: " + score + " klass: " + klass + " color: " + color)
+      
+          let [y1, x1, y2, x2] = boxes_data.slice(i * 4, (i + 1) * 4);
+          x1 *= ratios[0];
+          x2 *= ratios[0];
+          y1 *= ratios[1];
+          y2 *= ratios[1];
+          const width = x2 - x1;
+          const height = y2 - y1;
+
+          console.log("width: " + width + " height: " + height)
+
+          // draw box.
+          ctx.fillStyle = Colors.hexToRgba(color, 0.2);
+          ctx.fillRect(x1, y1, width, height);
+      
+          // draw border box.
+          ctx.strokeStyle = color;
+          ctx.lineWidth = Math.max(Math.min(ctx.canvas.width, ctx.canvas.height) / 200, 2.5);
+          ctx.strokeRect(x1, y1, width, height);
+      
+          // Draw the label background.
+          ctx.fillStyle = color;
+          const textWidth = ctx.measureText(klass + " - " + score + "%").width;
+          const textHeight = parseInt(font, 10); // base 10
+          const yText = y1 - (textHeight + ctx.lineWidth);
+          ctx.fillRect(
+            x1 - 1,
+            yText < 0 ? 0 : yText, // handle overflow label box
+            textWidth + ctx.lineWidth,
+            textHeight + ctx.lineWidth
+          );
+      
+          // Draw labels
+          ctx.fillStyle = "#ffffff";
+          ctx.fillText(klass + " - " + score + "%", x1 - 1, yText < 0 ? 0 : yText);
+        }
+      };
+      
+      class Colors {
+        // ultralytics color palette https://ultralytics.com/
+        constructor() {
+          this.palette = [
+            "#FF3838",
+            "#FF9D97",
+            "#FF701F",
+            "#FFB21D",
+            "#CFD231",
+            "#48F90A",
+            "#92CC17",
+            "#3DDB86",
+            "#1A9334",
+            "#00D4BB",
+            "#2C99A8",
+            "#00C2FF",
+            "#344593",
+            "#6473FF",
+            "#0018EC",
+            "#8438FF",
+            "#520085",
+            "#CB38FF",
+            "#FF95C8",
+            "#FF37C7",
+          ];
+          this.n = this.palette.length;
+        }
+      
+        get = (i) => this.palette[Math.floor(i) % this.n];
+      
+        static hexToRgba = (hex, alpha) => {
+          var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+          return result
+            ? `rgba(${[parseInt(result[1], 16), parseInt(result[2], 16), parseInt(result[3], 16)].join(
+                ", "
+              )}, ${alpha})`
+            : null;
+        };
+      }
+
+
     const preprocess = (source, modelWidth, modelHeight) => {
         let xRatio, yRatio; // ratios for boxes
 
@@ -123,8 +223,8 @@ const YOLOv8_TFJS = async (box) => {
             return [];
         }
 
-        console.log(model);
-        console.log(model.inputs);
+        // console.log(model);
+        // console.log(model.inputs);
 
         const modelWidth = modelDetails.inputShape[1];
         const modelHeight = modelDetails.inputShape[2];
@@ -185,24 +285,12 @@ const YOLOv8_TFJS = async (box) => {
             };
         });
 
-        console.log(boundingBoxes); // add this line
+        // console.log(boundingBoxes); // add this line
 
 
         tf.dispose([input, res, predictions, boxes, scores, classes, nms]);
 
-        boundingBoxes.forEach(result => {
-            ctx.beginPath();
-            ctx.rect(result.bbox[0], result.bbox[1], result.bbox[2], result.bbox[3]);
-            ctx.lineWidth = 2;
-            ctx.strokeStyle = "red";
-            ctx.fillStyle = "red";
-            ctx.stroke();
-            ctx.fillText(
-                result.label,
-                result.bbox[0],
-                result.bbox[1] > 10 ? result.bbox[1] - 5 : 10
-            );
-        });
+        renderBoxes(canvas, boxes_data, scores_data, classes_data, [xRatio, yRatio]);
 
         return boundingBoxes;
     }
